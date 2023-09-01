a1382c28177d1508698632704c009c411e0609ad
Erick Rosa <erickpmotta@gmail.com>
2023-09-01 12:59:06 -0300

    Added data carving functionality

--- src/filesystem_analysis/fat_reader.py
+++ src/filesystem_analysis/fat_reader.py
@@ -22,3 +22,45 @@ class FATReader:
             # Add other metadata attributes as required
         }
             return metadata
+        
+
+def carve_files(self, output_directory):
+    
+    img = pytsk3.Img_Info(self.image_path)
+
+    # Initialize some variables
+    start_sector = 0
+    end_sector = img.info.media_size // img.info.block_size
+    is_jpeg = False
+    jpeg_data = b""
+
+    # Create the output directory if it doesn't exist
+    if not os.path.exists(output_directory):
+        os.mkdir(output_directory)
+    
+    file_count = 0
+
+    for sector in range(start_sector, end_sector):
+        # Read the sector
+        offset = sector * img.info.block_size
+        data = img.read_random(offset, img.info.block_size)
+
+        # Search for JPEG start signature
+        if data.startswith(b'\xFF\xD8'):
+            is_jpeg = True
+            jpeg_data = b""
+        
+        # If inside a JPEG, accumulate data
+        if is_jpeg:
+            jpeg_data += data
+
+        # Search for JPEG end signature
+        if data.endswith(b'\xFF\xD9'):
+            is_jpeg = False
+            file_count += 1
+            
+            # Save the JPEG data
+            with open(f"{output_directory}/carved_{file_count}.jpg", 'wb') as f:
+                f.write(jpeg_data)
+
+    print(f"Carved {file_count} JPEG files.")
