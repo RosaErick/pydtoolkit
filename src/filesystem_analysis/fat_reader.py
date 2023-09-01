055528a076f8ab2f768a47e3a4cd0493a0062b8b
Erick Rosa <erickpmotta@gmail.com>
2023-09-01 12:52:21 -0300

    Added file metadata extraction functionality

--- src/filesystem_analysis/fat_reader.py
+++ src/filesystem_analysis/fat_reader.py
@@ -10,3 +10,15 @@ class FATReader:
         directory = self.fs_info.open_dir(path=dir_path)
         for entry in directory:
             print(entry.info.name.name)
+
+    def get_file_metadata(self, file_path):
+            file = self.fs_info.open(file_path)
+            metadata = {
+            "name": file.info.name.name,
+            "size": file.info.meta.size,
+            "created_time": file.info.meta.crtime,
+            "accessed_time": file.info.meta.atime,
+            "modified_time": file.info.meta.mtime,
+            # Add other metadata attributes as required
+        }
+            return metadata
--- tests/test_fat_reader.py
+++ tests/test_fat_reader.py
@@ -7,5 +7,12 @@ class TestFATReader(unittest.TestCase):
         fat_reader = FATReader("path/to/fat/image")
         fat_reader.list_directory("/")  # Here you'd actually check expected outcomes
 
+
+    def test_get_file_metadata(self):
+        fat_reader = FATReader("path/to/fat/image")
+        metadata = fat_reader.get_file_metadata("/some_file.txt")
+        # Add assertions to check the metadata is as expected
+
+
 if __name__ == "__main__":
     unittest.main()
